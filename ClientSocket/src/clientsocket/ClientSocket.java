a6a3e270ee53c7972dffe6a3840349e20cf8a6f7
Giannese Mattia <giannese.mattia@osdbsesto>
2022-02-16 09:15:52 +0100

    rep

--- ClientSocket/src/clientsocket/ClientSocket.java
+++ ClientSocket/src/clientsocket/ClientSocket.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javasocket;
+package clientsocket;
 
 /**
  *
  * @author giannese.mattia
  */
-public class Javasocket {
+public class ClientSocket {
 
     /**
      * @param args the command line arguments
